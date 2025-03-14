c4adc76d95071f0e9acd910670f4f814f7c5e48a
Shubham  Saurav <shubhamsinha186@gmail.com>
2025-03-14 21:07:45 +0530

    Added database connection

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+# External Imports
 from flask import Flask
 
+# Internal Imports
+from db import db
+
 app = Flask(__name__)
+
+# Configuring the database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
