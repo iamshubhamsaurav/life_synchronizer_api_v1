77e5ddfb2be9c95416c488aad4d3e3cfa91d6c67
Shubham  Saurav <shubhamsinha186@gmail.com>
2025-03-15 00:34:25 +0530

    Fixed model - changed required to nullable

--- models/User.py
+++ models/User.py
@@ -6,15 +6,15 @@ class UserModel(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, required=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(200), unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
-    name = db.Column(db.String(80), required=True)
+    name = db.Column(db.String(80), nullable=False)
 
-    def __repr__(self):
-        return f'''<User - 
-        id: {self.id}>, 
-        username: {self.username}, 
-        email: {self.email}, 
-        password: {self.password}'''
+    # def __repr__(self):
+    #     return f'''<User - 
+    #     id: {self.id}>, 
+    #     username: {self.username}, 
+    #     email: {self.email}, 
+    #     password: {self.password}'''
     
