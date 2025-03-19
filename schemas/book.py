7a0e7c7a513b26893c87bee2961f49ff6caf64d8
Shubham  Saurav <shubhamsinha186@gmail.com>
2025-03-19 23:09:11 +0530

    Added Book Schema

---
+++ schemas/book.py
@@ -0,0 +1,17 @@
+from  marshmallow import fields, Schema
+
+class _BaseBookSchema(Schema):
+    id = fields.Int(dumy_only=True)
+    title = fields.Str(required=True)
+    author = fields.Str(required=True)
+    status = fields.Str(required=True)
+    start_date = fields.DateTime()
+    finish_date = fields.DateTime() 
+    user_id = fields.Integer(required=True)
+
+class CreateBookSchema(_BaseBookSchema):
+    pass
+
+class ReadNoteSchema(_BaseBookSchema):
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
