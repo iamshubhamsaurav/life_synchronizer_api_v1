56a422fb48896271d6d3c2b3a66b8e8b85cc2aff
Shubham  Saurav <shubhamsinha186@gmail.com>
2025-07-14 12:55:37 +0530

    Code documentation added

--- resources/recents.py
+++ resources/recents.py
@@ -28,30 +28,35 @@ blp = Blueprint("recents", "recents", url_prefix="/api/v1/recents")
 class BookList(MethodView):
     @blp.response(200, ReadBookSchema(many=True))
     def get(self):
+        '''Get 5 recent books.'''
         recent_records = BookModel.query.order_by(BookModel.created_at.desc()).limit(5).all()
         return recent_records
     
 @blp.get("/entertainments/")
 @blp.response(200, ReadEntertainmentSchema(many=True))
 def get_recent_entertainments():
+    '''Get 5 recent expense entertainments.'''
     recent_records = EntertainmentModel.query.order_by(EntertainmentModel.created_at.desc()).limit(5).all()
     return recent_records
 
 @blp.get("/notes/")
 @blp.response(200, ReadNoteSchema(many=True))
 def get_recent_notes():
+    '''Get 5 recent notes.'''
     recent_records = NoteModel.query.order_by(NoteModel.created_at.desc()).limit(5).all()
     return recent_records
 
 @blp.get("/tasks/")
 @blp.response(200, ReadTaskSchema(many=True))
-def get_recent_notes():
+def get_recent_tasks():
+    '''Get 5 recent tasks.'''
     recent_records = TaskModel.query.order_by(TaskModel.created_at.desc()).limit(5).all()
     return recent_records
 
 @blp.get("/transactions/income/")
 @blp.response(200, ReadTransactionSchema(many=True))
 def get_recent_income_transaction():
+    '''Get 5 recent income transactions.'''
     result = []
     recent_records = TransactionModel.query.order_by(TransactionModel.created_at.desc()).limit(5).all()
     for record in recent_records:
@@ -62,6 +67,7 @@ def get_recent_income_transaction():
 @blp.get("/transactions/expense/")
 @blp.response(200, ReadTransactionSchema(many=True))
 def get_recent_expense_transaction():
+    '''Get 5 recent expense transactions.'''
     result = []
     recent_records = TransactionModel.query.order_by(TransactionModel.created_at.desc()).limit(5).all()
     for record in recent_records:
--- resources/stats.py
+++ resources/stats.py
@@ -20,6 +20,7 @@ blp = Blueprint("stats", "stats", url_prefix="/api/v1/stats")
 
 @blp.get('/overview')
 def get_overview():
+    '''Get the total count of all the data items.'''
     result = {}
     
     books = BookModel.query.count()
@@ -38,6 +39,7 @@ def get_overview():
 
 @blp.get('/transactions/')
 def get_transaction_stats():
+    '''Get the transactions stats: total income amount, total expense amount and net amount.'''
     result = {}
     income_amount = 0
     expense_amount = 0
@@ -56,6 +58,7 @@ def get_transaction_stats():
 
 @blp.get('/transactions/count/')
 def get_transaction_count():
+    '''Get the count of income and expense transactions.'''
     result = {}
     income_count = 0
     expense_count = 0
